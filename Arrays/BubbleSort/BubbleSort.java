5eee31f2b8a8729a6c01287f271964a2c7b445f6
Sweta Thakare <143093330+Swetathakare@users.noreply.github.com>
2023-11-30 14:42:49 +0530

    Update BubbleSort.java

--- Arrays/BubbleSort/BubbleSort.java
+++ Arrays/BubbleSort/BubbleSort.java
@@ -1,1 +1,39 @@
+package practice;
+import java.util.*;
 
+public class BinarySearch {
+	static Scanner sc = new Scanner(System.in);
+
+	static void bubbleSort(int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
+
+				if (array[j] > array[j + 1]) {
+
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	static void printArray(int array[]) {
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String args[]) {
+
+		int array[] = { 23, 21, 2, 4, 3, 2212, 32, -22, 2321, 886, 54, 33 };
+
+		System.out.println("Original Array :");
+		printArray(array);
+		bubbleSort(array);
+		System.out.println("Sorted Array :");
+		printArray(array);
+	}
+}
